8f34bb9fdf4f4ad6e6f7d98c315d29066c44c189
evenhh <273118949@qq.com>
2017-05-24 17:57:15 +0800

    Create sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+from email.mime.text import MIMEText
+import smtplib
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# 输入Email地址和口令:
+from_addr = ''
+username=''
+password = ""
+# 输入SMTP服务器地址:
+smtp_server = ''
+# 输入收件人地址:
+to_addr = ''
+
+
+server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+server.login(username, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
